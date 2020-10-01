73232904569d91b94baf750927f927e98480a644
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-10-01 13:31:11 +0530

    Added duration

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import speech_recognition as sr
 r = sr.Recognizer()
 file = sr.AudioFile('one.wav')
 with file as source:
- audio = r.record(source)
+ audio = r.record(source,duration=5)
  result = r.recognize_google(audio,language='es')
 print(result)
