9566dd435a39cadd2679298d46db864f3ca5463d
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-10-01 13:41:09 +0530

    Added chrome path to open chromium driver

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import speech_recognition as sr
 r = sr.Recognizer()
 file = sr.AudioFile('one.wav')
 with file as source:
+ r.adjust_for_ambient_noise(source)
  audio = r.record(source,duration=5)
  result = r.recognize_google(audio,language='es')
 print(result)
