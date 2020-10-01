a9e54884dc20a31389707258674f162a70277bd1
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-10-01 13:21:53 +0530

    Used google cloud recognition

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import speech_recognition as sr
 r = sr.Recognizer()
 file = sr.AudioFile('one.wav')
+with file as source:
+ audio = r.record(source)
+ result = r.recognize_google(audio)
+print(result)
